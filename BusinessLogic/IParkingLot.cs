21753ef7fa2af9e574469ffa37d76d2b96382b22
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-08-24 11:58:02 +0200

    Added IParkingLot interface

---
+++ BusinessLogic/IParkingLot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    interface IParkingLot
+    {
+        void LeaveParkingLot(Car car);
+        void ArriveAtParkingLot(Car car);
+        Car FindCar(Car car);
+        IReadOnlyList<Car> AllCars();
+    }
+}

bd1a70a7823165216e851707818431f28ab53f67
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-08-24 11:03:39 +0200

    Implemented DCD

--- BusinessLogic/ParkingLot.cs
+++ BusinessLogic/ParkingLot.cs
@@ -8,5 +8,26 @@ namespace BusinessLogic
 {
     class ParkingLot
     {
+        private List<Car> _cars = new List<Car>();
+
+        public void LeaveParkingLot(Car car)
+        {
+            _cars.Remove(car);
+        }
+
+        public void ArriveAtParkingLot(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public Car FindCar(Car car)
+        {
+            return _cars.Find((carInList) => carInList == car);
+        }
+
+        public IReadOnlyList<Car> AllCars()
+        {
+            return _cars;
+        }
     }
 }

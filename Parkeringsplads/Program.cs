6b6519a16e2f712068cdd8cfef2c54194c772ec7
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-08-24 11:59:04 +0200

    Added test functionality

--- Parkeringsplads/Program.cs
+++ Parkeringsplads/Program.cs
@@ -2,16 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using BusinessLogic;
 
 namespace Parkeringsplads
 {
-    class Program
+    public class Program
     {
+        public static bool shouldSleep = true;
+        public static int sleepInterval = 500;
+        Random random = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            ParkingLot parkingLot = new ParkingLot();
+            Driver driver1 = new Driver(parkingLot);
+            Thread.Sleep(sleepInterval);
+            Driver driver2 = new Driver(parkingLot);
+            Thread.Sleep(sleepInterval);
+            Driver driver3 = new Driver(parkingLot);
+            Thread.Sleep(sleepInterval);
+            Driver driver4 = new Driver(parkingLot);
+            Thread.Sleep(sleepInterval);
+            Driver driver5 = new Driver(parkingLot);
+            Thread.Sleep(sleepInterval);
+            Viewer viewer1 = new Viewer(parkingLot);
+
+            driver1.task.Wait();
         }
     }
 }
